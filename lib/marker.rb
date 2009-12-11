fee9806ad577dac6403ae44e9f9527791047142b
Gregg Pollack <gregg@sourfamily.com>
2009-12-10 20:40:09 -0500

    Phat Ass commit

--- lib/marker.rb
+++ lib/marker.rb
@@ -1,8 +1,26 @@
 class Marker
+
   def initialize(secret)
+    @secret = secret.split(" ")
+    @non_positional_matches = 0
+    @marker = []
   end
+  
   def guess(these)
+    @guess = these.split(" ")
   end
+  
   def mark
+    @guess.each_with_index do |g, i| 
+      if @secret.include? g
+        if @secret[i] == g
+          @marker.push 'p'
+        else
+          @marker.push 'm'
+        end
+      end
+    end
+    @marker
   end
+
 end
--- spec/marker_spec.rb
+++ spec/marker_spec.rb
@@ -1,2 +1,97 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
+describe Marker do
+  context "No matches" do
+    it "Should return []" do
+      m = Marker.new('r y g b')
+      m.guess('c c c c')
+      m.mark.should == []    
+    end
+  end
+  
+  context "1 non-positional match" do
+    it "Should return ['m']" do
+      m = Marker.new('r y g b')
+      m.guess('b c c c')
+      m.mark.should == ['m']    
+    end
+    
+    it "Should also return ['m']" do
+      m = Marker.new('r y g b')
+      m.guess('c b c c')
+      m.mark.should == ['m']    
+    end
+  end
+  
+  context "2 non-positional matches" do
+    it "Should return ['m', 'm']" do
+      m = Marker.new('r y g b')
+      m.guess('b g c c')
+      m.mark.should == ['m', 'm']    
+    end
+  end
+  
+  context "3 non-positional matches" do
+    it "Should return ['m', 'm', 'm']" do
+      m = Marker.new('r y g b')
+      m.guess('b g r c')
+      m.mark.should == ['m', 'm', 'm']    
+    end
+  end
+  
+  context "4 non-positional matches" do
+    it "Should return ['m', 'm', 'm', 'm']" do
+      m = Marker.new('r y g b')
+      m.guess('b g r y')
+      m.mark.should == ['m', 'm', 'm', 'm']    
+    end
+  end
+
+  context "1 positional match" do
+    it "Should return ['p']" do
+      m = Marker.new('r y g b')
+      m.guess('r c c c')
+      m.mark.should == ['p']    
+    end
+  end
+  
+  context "2 positional matches" do
+    it "Should return ['p','p']" do
+      m = Marker.new('r y g b')
+      m.guess('r y c c')
+      m.mark.should == ['p','p']    
+    end
+  end
+  
+  context "3 positional matches" do
+    it "Should return ['p','p']" do
+      m = Marker.new('r y g b')
+      m.guess('r y g c')
+      m.mark.should == ['p','p','p']    
+    end
+  end
+  
+  context "4 positional matches" do
+    it "Should return ['p','p']" do
+      m = Marker.new('r y g b')
+      m.guess('r y g b')
+      m.mark.should == ['p','p','p','p']    
+    end
+  end
+  
+  context "1 positional match and 1 non-positional match" do
+    it "Should return ['m','p']" do
+      m = Marker.new('r y g b')
+      m.guess('g y c c')
+      m.mark.should == ['m', 'p']    
+    end
+  end
+  
+  context "2 positional match and 2 non-positional match" do
+    it "Should return ['p','p','m','m']" do
+      m = Marker.new('r y g b')
+      m.guess('r y b g')
+      m.mark.should == ['p','p','m','m']    
+    end
+  end
+end
